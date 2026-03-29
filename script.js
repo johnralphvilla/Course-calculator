134c40cc78e5157a3f1745297ee607df6b19f514
johnralphvilla <ralphjohnagustin4@gmail.com>
2026-03-29 00:56:55 +0000

    1

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ function generateInputs() {
     `;
   }
 }
+function getGPA(mark) {
+  if (mark >= 90) return 4.0;
+  if (mark >= 80) return 3.7;
+  if (mark >= 70) return 3.0;
+  if (mark >= 60) return 2.0;
+  if (mark >= 50) return 1.0;
+  return 0;
+}
+
+
+function calculate() {
+  const inputs = document.querySelectorAll(".marks");
+
+  let total = 0;
+  let totalGPA = 0;
+  let count = inputs.length;
+
+  inputs.forEach(input => {
+    const val = Number(input.value);
+
+    if (!isNaN(val)) {
+      total += val;
+      totalGPA += getGPA(val);
+    }
+  });
+
+  const avg = total / count;
+  const gpa = totalGPA / count;
+
+  document.getElementById("result").textContent =
+    `Average: ${avg.toFixed(2)} | GPA: ${gpa.toFixed(2)} / 4.0`;
+}
+
+
+
+
+
